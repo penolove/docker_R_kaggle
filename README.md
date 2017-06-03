5c036fbe2bacbd5bce97ade64bd5d8ead1c935ab
stream <penolove19.am97@g2.nctu.edu.tw>
2017-06-03 15:51:31 +0800

    edit script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # docker R with kaggle's image
 kaggle/rstats
+the login username/password is rstudio/rstudio.
+
+you might need to edit the mount folder in run_server.sh
+
+
+```
+#start the server
+./run_server.sh;
+#now you can visit your ip:8787 to check if service start
+
+#stop the server
+./stop_server.sh
+
+```
--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,5 @@
 docker run --net host --name R_s \
         -v /home/stream/Documents/:/home/rstudio/Documents/ \
-        -d kaggle/rstats tail -f /dev/null
+        -d kaggle/rstats /bin/bash -c "rstudio-server restart & tail -f /dev/null";
 #sudo ./pipework.sh
 #        -v /home/stream/Documents/:/home/rstudio/Documents/ \
